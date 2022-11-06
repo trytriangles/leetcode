2485948d831d773f914b78f50e2383cc5754129a
Ryan Plant <ryan@ryanplant.net>
2022-11-06 07:42:20 +0000

    Refactor

--- two-sum-2.cpp
+++ two-sum-2.cpp
@@ -3,8 +3,9 @@ public:
     vector<int> twoSum(vector<int>& numbers, int target) {
         int left = 0;
         int right = numbers.size() - 1;
+        int summed = 0;
         while (left < right) {
-            int summed = numbers[left] + numbers[right];
+            summed = numbers[left] + numbers[right];
             if (summed == target) return vector<int>{ left + 1, right + 1};
             if (summed < target) {
                 ++left;
