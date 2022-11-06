f210e66eb06a79f1987f29915242d076f41da336
Ryan Plant <ryan@ryanplant.net>
2022-11-06 06:54:30 +0000

    Improve memory usage

--- min-cost-climbing-stairs.go
+++ min-cost-climbing-stairs.go
@@ -9,18 +9,15 @@ func minCostClimbingStairs(cost []int) int {
 	if len(cost) == 2 {
 		return min(cost[0], cost[1])
 	}
-	var minimums = make([]int, len(cost))
-	minimums[0] = 0 // Costs 0 to start at 0.
-	minimums[1] = 0 // Costs 0 to start at 1.
-	// Reaching 2 has the cost of departing from the cheaper starting point.
-	minimums[2] = min(cost[0], cost[1])
+	y := 0                     // Cost of reaching pos 1.
+	z := min(cost[0], cost[1]) // Cost of reaching pos 2.
 	for i := 3; i < len(cost); i++ {
-		a := cost[i-1] + minimums[i-1]
-		b := cost[i-2] + minimums[i-2]
-		minimums[i] = min(a, b)
+		a := cost[i-1] + z
+		b := cost[i-2] + y
+		z, y = min(a, b), z
 	}
 	top := len(cost)
-	a := cost[top-1] + minimums[top-1]
-	b := cost[top-2] + minimums[top-2]
+	a := cost[top-1] + z
+	b := cost[top-2] + y
 	return min(a, b)
 }
