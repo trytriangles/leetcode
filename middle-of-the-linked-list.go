bf1165b395311ca7246a11a0b16afb6bb15597af
Ryan Plant <ryan@ryanplant.net>
2022-11-10 14:10:05 +0000

    Re-solve using a much more memory-efficient two-pointer approach

--- middle-of-the-linked-list.go
+++ middle-of-the-linked-list.go
@@ -1,13 +1,20 @@
-var visited = []*ListNode{}
-
+// middleNode returns the middle node of a linked list. If there are two middle
+// nodes (i.e. the length of the list is an even number), the second one is
+// returned.
 func middleNode(head *ListNode) *ListNode {
-	visited = append(visited, head)
+	// Short circuit: for a list of one element, the middle is that element.
 	if head.Next == nil {
-		middle := (len(visited) / 2)
-		var result = visited[middle]
-		visited = []*ListNode{}
-		return result
+		return head
 	}
-	return middleNode(head.Next)
-}
 
+	// Iterate the list using two pointers; oneCounter will move one step at a
+	// time, twoCounter two steps at a time. When twoCounter points to the end,
+	// oneCounter will point to the middle.
+	oneCounter := head
+	twoCounter := head
+	for twoCounter != nil && twoCounter.Next != nil {
+		oneCounter = oneCounter.Next
+		twoCounter = twoCounter.Next.Next
+	}
+	return oneCounter
+}
